f0f85d35cae5026b6f28104fddaecff90835db42
Luis Blanco <luisblanco@Luiss-MacBook-Pro.local>
2022-07-02 09:25:25 -0500

    Images in card look good

--- src/pages/project-list/ProjectList.jsx
+++ src/pages/project-list/ProjectList.jsx
@@ -4,12 +4,10 @@ import { Project } from '../project/Project';
 export const ProjectList = () => {
     return (
         <div className='flex flex-col align-middle  gap-2 flex-wrap m-4 w-96'>
-            <Project name='project 1' />
-            <Project name='project 2' />
-            <Project name='project 3' />
-            <Project name='project 4' />
-            <Project name='project 5' />
-            <Project name='project 6' />
+            <Project name='project 1' imageNumber={1} />
+            <Project name='project 2' imageNumber={2} />
+            <Project name='project 3' imageNumber={3} />
+            <Project name='project 4' imageNumber={4} />
         </div>
     );
 };
--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Card } from 'react-daisyui';
 
-export const Project = ({ name }) => {
+export const Project = ({ name, imageNumber }) => {
     return (
-        <Card className='p-4'>
+        <Card className='p-4 h-96'>
             <Card.Title className='text-bold'>{name}</Card.Title>
-            <Card.Image></Card.Image>
-            <Card.Body>
+            <Card.Image className='mt-4 w-80 h-36' src={`/images/construction_${imageNumber}.jpg`}></Card.Image>
+            <Card.Body className='p-4'>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio
                 alias possimus, eligendi tempora doloribus id. Deleniti suscipit
-                eos, animi, incidunt at, fugiat alias sapiente ipsam voluptates
-                quasi rerum! Unde error, officiis dolor magni maiores voluptatem
-                vero, ullam nesciunt, ipsam sint corporis similique
+                eos, animi, incidunt at,
                 consequuntur!.
             </Card.Body>
         </Card>
