6586e0ced0279c012e5853078ca38c75cb9e6636
Luis Blanco <luisblanco@Luiss-MacBook-Pro.local>
2022-07-02 08:41:26 -0500

    Footer works

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,19 +1,22 @@
 import './../../css/index.css';
 
-import { Footer } from 'react-daisyui';
-
 import { CustomNavbar } from './../navbar/CustomNavbar';
 import { ProjectList } from '../../pages/project-list/ProjectList';
+import { CustomFooter } from '../footer/CustomFooter';
 
 function App() {
     return (
         <>
-            <div className='flex flex-col items-stretch h-screen'>
-                <CustomNavbar />
-                <main className='container flex-1 bg-gray-100 text-black mx-auto h-full'>
+            <div className='flex flex-col items-stretch justify-around h-screen overflow-auto'>
+                <header>
+                    <CustomNavbar />
+                </header>
+                <main className=' flex  bg-gray-100 text-black mx-auto h-full w-full'>
                     <ProjectList />
                 </main>
-                <Footer className='bg-yellow-300 h-4' />
+                <footer>
+                    <CustomFooter className='' />
+                </footer>
             </div>
         </>
     );
---
+++ src/components/footer/CustomFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Footer } from 'react-daisyui';
+
+export const CustomFooter = () => {
+    return (
+        <div>
+            <Footer className='bg-black h-12'>Hello, Footer!</Footer>
+        </div>
+    );
+};
