2773ceeaa15d967efe671af412e49c192c8ea52c
Luis Blanco <luisblanco@Luiss-MacBook-Pro.local>
2022-07-02 07:36:32 -0500

    Added tailwind, react router, daisyui

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import './../../css/index.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <h1 className='container bg-gray-100 text-black mx-auto'>
+                Hello, World!
+            </h1>
+        </div>
+    );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./src/**/*.{html,js,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                customDarkGreen: '#076140',
+                customLightGray: '#9BB0A4',
+                customDarkGray: '#687B71',
+                customDarkBlue: '#005D89',
+                customMediumBlue: '#298FBF',
+            },
+        },
+    },
+    safelist: [
+        {
+            pattern: /./,
+        },
+    ],
+    plugins: [require('daisyui')],
+};
